b438f59f6980b9b77cd901d78de44667fcad58f4
James Lucassen <james@Jamess-MacBook-Pro.local>
2024-01-14 11:05:35 +0000

    basic reporting function

---
+++ metric.py
@@ -0,0 +1,45 @@
+import tiktoken
+from openai import OpenAI
+import os
+import colorama
+
+def report_success_LLM(response: str, task: str, print_output=True, model: str = 'gpt-4', reporter_prompt = None) -> bool:
+    model = "gpt-4"
+    client = OpenAI(api_key = os.getenv('OPENAI_API_KEY'))
+
+    default_reporter_prompt = "This is the output of a GPT-4 instance that was assigned a task. Your job is to report whether it completed that task successfully. Respond with either <YES> or <NO>. The task was:\n"
+
+    report = client.chat.completions.create(
+    model=model,
+    messages=[
+        {
+            "role": "system",
+            "content": "",
+        },
+        {"role": "user", "content": default_reporter_prompt+task if not reporter_prompt else reporter_prompt+task}
+    ],
+    temperature=0.9,
+    max_tokens=512,
+    top_p=0.98,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stream=True,
+    )
+
+    if print_output: print(colorama.Fore.BLUE + '\n\nStarting LLM Report:\n\n')
+
+    report_string = ''
+    for chunk in report:
+        content = chunk.choices[0].delta.content
+        if content:
+            report_string += chunk.choices[0].delta.content
+            if print_output: print(chunk.choices[0].delta.content, end="", flush=True)
+
+    if print_output: print(colorama.Style.RESET_ALL)
+
+    if '<YES>' in report_string and '<NO>' not in report_string:
+        return True
+    elif '<NO>' in report_string and '<YES>' not in report_string:
+        return False
+    else:
+        return None
