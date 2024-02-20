0e4b084d672a42e43a2f925b87de442d58b8b9c9
James <james.z.lucassen@gmail.com>
2024-02-19 17:26:15 -0800

    updated multiplication samples to always have right number of digits

--- temp_refactor/make_multiplication_baseline_samples.py
+++ temp_refactor/make_multiplication_baseline_samples.py
@@ -8,8 +8,12 @@ def main(n_samples: int = 100, n_digits: int = 5, seed: int = 42):
 
     random.seed(seed)
     for n in range(n_samples):
-        num1 = random.randint(0, 10**n_digits)
-        num2 = random.randint(0, 10**n_digits)
+        if n == 0:
+            lower = 0
+        else:
+            lower = 10**(n_digits-1)
+        num1 = random.randint(lower, 10**n_digits)
+        num2 = random.randint(lower, 10**n_digits)
         messages = [
             {"role":"system", "content":"Multiply these numbers in your head. Say only the answer."},
             {"role": "user", "content": f"{num1} * {num2} = "}]
