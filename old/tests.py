35bad3c56db7390a3a833573233f2eb5813d43d9
James <james.z.lucassen@gmail.com>
2024-03-06 14:18:52 -0800

    don't need "old", that's what version control is for

--- old/tests.py
+++
@@ -1,9 +0,0 @@
-import metric
-
-assert metric.evaluate_response("1 2  4 5", "Count from 1 to 5") == False
-
-assert metric.evaluate_response("1 2 2 4 5", "Count from 1 to 5") == False
-
-assert metric.evaluate_response("1 2 3 4 5", "Count from 1 to 5") == True
-
-assert metric.evaluate_response("1 2 three 4 5", "Count from 1 to 5") == True
